0bf430cb258fe9ad3dcf83c3d538521531f10274
ABGUHA-LAP\abguha <abhilash.guha@oracle.com>
2019-04-04 02:38:13 +0530

    FC

---
+++ SortingAlgorithms/src/javaprep/sort/demo/MergeSort.java
@@ -0,0 +1,75 @@
+package javaprep.sort.demo;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		
+		MergeSort mergeSort = new MergeSort();
+		Integer[] array = {8,2,5,1,3,7,9,6,4};
+		mergeSort.mergeSort(array);
+		for(int i:array) {
+			System.out.print(i+" ");
+		}
+	}
+	// The steps to mergesort are:
+	// 1. Break the Array into subarrays
+	// 2. While Merging two arrays into one array and make sure it's sorted
+	
+	// First we start from the second point we write the merging logic first
+	// Mind you: lArray and rArray are already sorted arrays
+	// As merge is called recursively last, hence both left and right array start with one elements,
+	// and get sorted on their way up, making sure all the times the lArrays and the rArray are sorted
+	void merge(Integer[] lArray, Integer[] rArray, Integer[] array){
+		
+		// First the strategy will be to compare each element of the right array
+		// with each element of the right array, as each array is sorted within itself.
+		int rLength = rArray.length;
+		int lLength = lArray.length;
+		int mainPointer = 0;
+		int rPointer = 0;
+		int lPointer = 0;
+		// While loop to compare the two arrays
+		while(rPointer < rLength && lPointer < lLength) {
+			if(lArray[lPointer]<rArray[rPointer]) {
+				array[mainPointer] = lArray[lPointer];
+				lPointer++;
+			}else if(rArray[rPointer]<lArray[lPointer]) {
+				array[mainPointer] = rArray[rPointer];
+				rPointer++;
+			}
+			mainPointer++;
+			
+		}// While loop to check if elements are left in lArray
+		while(lPointer<lLength) {
+			array[mainPointer]=lArray[lPointer];
+			lPointer++;
+			mainPointer++;
+		}// While loop to check if elements are left in rArray
+		while(rPointer<rLength) {
+			array[mainPointer]=rArray[rPointer];
+			rPointer++;
+			mainPointer++;
+		}		
+	}
+	
+	void mergeSort(Integer[] array) {
+		
+		int length = array.length;
+		if(length<2) {
+			return;
+		}
+		int mid = length/2;
+		Integer[] left = new Integer[mid];
+		Integer[] right = new Integer[length - mid];
+		for(int i=0;i<mid;i++) {
+			left[i] = array[i];
+		}
+		for(int j=mid,i=0;j<length;j++,i++){
+			right[i] = array[j];
+		}
+		mergeSort(left);
+		mergeSort(right);
+		merge(left, right, array);
+	}
+
+}
---
+++ SortingAlgorithms/src/javaprep/sort/demo/QuickSort.java
@@ -0,0 +1,64 @@
+package javaprep.sort.demo;
+
+public class QuickSort {
+
+	public static void main(String[] args) {
+		
+		QuickSort quickSort = new QuickSort();
+		Integer[] array = {8,2,5,1,3,7,9,6,4};
+		quickSort.quickSort(array, 0, array.length-1);
+		for(int i:array) {
+			System.out.print(i+" ");
+		}
+
+	}
+	
+	// Quick sort is following the below steps:
+	// 1. Find the pivot node(assume it to be the last element of any array)
+	// 2. Arrange all the elements smaller than it in it's left and all greater in it's right
+	// 3. Keep breaking the array into i=0->pivot and pivot+1->n
+	// 4. Mind you this is a in-place algorithm, hence we do not create any subarrays, so we do not need to
+	// merge this thing back to get results
+	
+	// The recursive method will thus have a start and end of the array to understand which part of it is
+	// getting worked on
+	void quickSort(Integer[] array, int start, int end) {
+		
+		if(start>=end)
+			return;
+		int pivotIndex = partition(array, start, end);
+		quickSort(array, start,pivotIndex-1);
+		quickSort(array,pivotIndex+1, end);
+	}
+	
+	// This will have to partition the array.
+	// Find the pivot node and then put all smaller in left and all larger in right
+	// return pivot node
+	int partition(Integer[] array, int start, int end) {
+		int pivotValue = array[end];
+		int pivotIndex = start; // this is initialized to start as when the next condition gets fullfilled there
+		// is only one item in the sub array and start has to act as the pivot then
+		for(int i=start;i<end;i++) {
+			if(array[i]<=pivotValue) {
+				// swap i and pivot index
+				int temp = array[i];
+				array[i] = array[pivotIndex];
+				array[pivotIndex] = temp;
+				pivotIndex++;
+				// increment Pivot index with every match found for lessor than pivot
+				
+			}
+		}
+		// Swap pivotIndex and end index
+		int temp = array[pivotIndex];
+		array[pivotIndex] = array[end];
+		array[end] = temp;
+		
+		
+		return pivotIndex;
+		
+	}
+	
+	
+
+}
